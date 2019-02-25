37f882c72586d2ae168c127ba0d87e5b139f4495
Richard Vizcarra <Richard Vizcarra@LAPTOP-A9SK120E>
2019-02-25 00:42:36 -0700

    fisrt version

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,48 @@
+package cse360assign2;
+/*
+ * Author: Richard Vizcarra
+ * Class ID: 577
+ * Assignment 2
+ * 
+ * The purpose of the program is to have the calculator class
+ * get the total, add to total, subtract from total, multiply
+ * total, divide total and get history
+ */
+public class Calculator 
+{
+private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	public void add (int value) 
+	{
+		
+	}
+	
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	public void divide (int value) 
+	{
+		
+	}
+	
+	public String getHistory () 
+	{
+		return "";
+	}
+}
