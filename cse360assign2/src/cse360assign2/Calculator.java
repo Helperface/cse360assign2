d9a418dd24b42b43585442b441f367fea545ef88
Richard Vizcarra <Richard Vizcarra@LAPTOP-A9SK120E>
2019-02-25 00:46:28 -0700

    version 3

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -11,7 +11,8 @@ package cse360assign2;
 public class Calculator 
 {
 private int total;
-	
+private String history="0";
+
 	public Calculator () 
 	{
 		total = 0;  // not needed - included for clarity
@@ -25,16 +26,19 @@ private int total;
 	public void add (int value) 
 	{
 		total = total + value;
+		history = history + " + " + value;
 	}
 	
 	public void subtract (int value)
 	{
 		total = total - value;
+		history = history + " - " + value;
 	}
 	
 	public void multiply (int value) 
 	{
 		total = total * value;
+		history = history + " * " + value;
 	}
 	
 	public void divide (int value) 
@@ -46,6 +50,7 @@ private int total;
 		else
 		{
 			total = total / value;
+			history = history + " / " + value;
 		}
 	}
 	
